d32ab4520031f534ab876a832dc10e0967b57932
berke-esmer-2015400021 <berke.esmer@boun.edu.tr>
2019-04-30 13:49:29 +0300

    ApiURL is the store that holds all urls.

---
+++ app/src/main/java/com/berkethetechnerd/surveypwp/helper/ApiURL.java
@@ -0,0 +1,11 @@
+package com.berkethetechnerd.surveypwp.helper;
+
+/**
+ * All constants are stored in this class.
+ */
+public class ApiURL {
+
+    public static final String API_BASE_URL = "http://45.76.39.46:5000/";
+    public static final String API_ALL_QUESTIONNAIRE = API_BASE_URL + "api/";
+
+}

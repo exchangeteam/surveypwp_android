a1274495894150980e23cb7608fa6f0b6cc99bc8
berke-esmer-2015400021 <berke.esmer@boun.edu.tr>
2019-04-30 14:51:18 +0300

    Questionnaire resource url is changed.

--- app/src/main/java/com/berkethetechnerd/surveypwp/helper/ApiURL.java
+++ app/src/main/java/com/berkethetechnerd/surveypwp/helper/ApiURL.java
@@ -6,6 +6,6 @@ package com.berkethetechnerd.surveypwp.helper;
 public class ApiURL {
 
     public static final String API_BASE_URL = "http://45.76.39.46:5000/";
-    public static final String API_ALL_QUESTIONNAIRE = API_BASE_URL + "api/";
+    public static final String API_ALL_QUESTIONNAIRE = API_BASE_URL + "api/questionnaires/";
 
 }

ae74cb9498a3e132df445dee88f36db75ac88e31
berke-esmer-2015400021 <berke.esmer@boun.edu.tr>
2019-04-30 13:38:17 +0300

    Our application's API functions will lay in this SurveyAPI.

---
+++ app/src/main/java/com/berkethetechnerd/surveypwp/ws/SurveyAPI.java
@@ -0,0 +1,9 @@
+package com.berkethetechnerd.surveypwp.ws;
+
+import com.berkethetechnerd.surveypwp.ApplicationClass;
+
+public class SurveyAPI {
+
+    // The coreApi instance for the application.
+    private static CoreApi coreApi = ApplicationClass.getCoreApi();
+}

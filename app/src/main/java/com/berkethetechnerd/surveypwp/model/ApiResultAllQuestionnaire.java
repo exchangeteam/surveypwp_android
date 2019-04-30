6a7518d4d9b1a06c0662513675b4825c3a554145
berke-esmer-2015400021 <berke.esmer@boun.edu.tr>
2019-04-30 14:52:03 +0300

    Questionnaire resource returns an array. This is added into model

--- app/src/main/java/com/berkethetechnerd/surveypwp/model/ApiResultAllQuestionnaire.java
+++ app/src/main/java/com/berkethetechnerd/surveypwp/model/ApiResultAllQuestionnaire.java
@@ -5,17 +5,17 @@ import com.google.gson.annotations.Expose;
 public class ApiResultAllQuestionnaire {
 
     @Expose
-    private ModelQuestionnaire items;
+    private ModelQuestionnaire[] items;
 
-    public ApiResultAllQuestionnaire(ModelQuestionnaire items) {
+    public ApiResultAllQuestionnaire(ModelQuestionnaire[] items) {
         this.items = items;
     }
 
-    public ModelQuestionnaire getItems() {
+    public ModelQuestionnaire[] getItems() {
         return items;
     }
 
-    public void setItems(ModelQuestionnaire items) {
+    public void setItems(ModelQuestionnaire[] items) {
         this.items = items;
     }
 }

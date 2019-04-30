a1f415679169fe6065019b34f3ca4355ed87e965
berke-esmer-2015400021 <berke.esmer@boun.edu.tr>
2019-04-30 13:39:35 +0300

    ApplicationClass is implemented.

---
+++ app/src/main/java/com/berkethetechnerd/surveypwp/ApplicationClass.java
@@ -0,0 +1,41 @@
+package com.berkethetechnerd.surveypwp;
+
+import android.app.Application;
+
+import com.berkethetechnerd.surveypwp.ws.CoreApi;
+
+import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
+
+/**
+ * This class contains the features that will be used throughout the entire application.
+ */
+public class ApplicationClass extends Application {
+
+    // The Api that which will handle network actions and requests.
+    private static CoreApi coreApi;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // Initialize the core Api.
+        coreApi = new CoreApi(getApplicationContext());
+
+        // Initialize the default font.
+        CalligraphyConfig.initDefault(new CalligraphyConfig.Builder()
+                .setDefaultFontPath("fonts/HelveticaNeue.ttf")
+                .setFontAttrId(R.attr.fontPath)
+                .build()
+        );
+    }
+
+    /**
+     * Gets the core Api that will be used for network actions and requests.
+     * Avoids the thread interference and memory consistency errors.
+     *
+     * @return CoreApi: Synchronized for safety.
+     */
+    public static synchronized CoreApi getCoreApi(){
+        return coreApi.get();
+    }
+}

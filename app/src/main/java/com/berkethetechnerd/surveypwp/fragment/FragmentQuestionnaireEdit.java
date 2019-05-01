a716d622000ec77672fb8caa58e858c25c4b816a
berke-esmer-2015400021 <berke.esmer@boun.edu.tr>
2019-05-01 12:27:16 +0300

    FragmentQuestionnaireEdit design and implementation is done.

---
+++ app/src/main/java/com/berkethetechnerd/surveypwp/fragment/FragmentQuestionnaireEdit.java
@@ -0,0 +1,102 @@
+package com.berkethetechnerd.surveypwp.fragment;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.berkethetechnerd.surveypwp.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link FragmentQuestionnaireEdit.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link FragmentQuestionnaireEdit#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class FragmentQuestionnaireEdit extends Fragment {
+
+    @BindView(R.id.btn_editQuestionnaire) EditText etQuestionnaireID;
+
+    private OnFragmentInteractionListener mListener;
+
+    public FragmentQuestionnaireEdit() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @return A new instance of fragment FragmentQuestionnaireEdit.
+     */
+    public static FragmentQuestionnaireEdit newInstance() {
+        return new FragmentQuestionnaireEdit();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootView = inflater.inflate(R.layout.fragment_questionnaire_edit, container, false);
+        ButterKnife.bind(this, rootView);
+
+        return rootView;
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    @OnClick(R.id.btn_editQuestionnaire)
+    public void editQuestionnaire(View v) {
+        String id = etQuestionnaireID.getText().toString();
+        String msg = "You need to type an ID.";
+
+        if(id.isEmpty()) {
+            Toast.makeText(getContext(), msg, Toast.LENGTH_SHORT).show();
+        } else {
+            mListener.editQuestionnaire(Integer.valueOf(id));
+        }
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        void editQuestionnaire(int id);
+    }
+}

d00cce0354c14b49156630d50fe6653dc291e1d8
berke-esmer-2015400021 <berke.esmer@boun.edu.tr>
2019-05-01 18:11:42 +0300

    QuestionModel is implemented.

---
+++ app/src/main/java/com/berkethetechnerd/surveypwp/model/ModelQuestion.java
@@ -0,0 +1,58 @@
+package com.berkethetechnerd.surveypwp.model;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class ModelQuestion {
+
+    @Expose
+    private int id;
+
+    @Expose
+    private String title;
+
+    @Expose
+    private String description;
+
+    @SerializedName("@controls")
+    private HyperControls controls;
+
+    public ModelQuestion(int id, String title, String description, HyperControls controls) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.controls = controls;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public HyperControls getControls() {
+        return controls;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setControls(HyperControls controls) {
+        this.controls = controls;
+    }
+}

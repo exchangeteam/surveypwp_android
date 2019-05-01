0ea9af9316181d3fb237bcb9978b784b3bdd763e
berke-esmer-2015400021 <berke.esmer@boun.edu.tr>
2019-05-01 16:24:56 +0300

    Add questionnaire function is implemented.

--- app/src/main/java/com/berkethetechnerd/surveypwp/ws/SurveyAPI.java
+++ app/src/main/java/com/berkethetechnerd/surveypwp/ws/SurveyAPI.java
@@ -2,9 +2,15 @@ package com.berkethetechnerd.surveypwp.ws;
 
 import com.android.volley.Request;
 import com.android.volley.Response;
+import com.android.volley.request.JsonObjectRequest;
+import com.android.volley.request.StringRequest;
 import com.berkethetechnerd.surveypwp.ApplicationClass;
 import com.berkethetechnerd.surveypwp.helper.ApiURL;
 import com.berkethetechnerd.surveypwp.model.ApiResultAllQuestionnaire;
+import com.berkethetechnerd.surveypwp.model.ApiResultNoData;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -23,6 +29,25 @@ public class SurveyAPI {
 
         GsonRequest<ApiResultAllQuestionnaire> request = new GsonRequest<>(Request.Method.GET, URL,
                 ApiResultAllQuestionnaire.class, headers, params, successListener, errorListener);
+
+        coreApi.getRequestQueue().getCache().clear();
+        coreApi.addToRequestQueue(request);
+    }
+
+    public static void addQuestionnaire(final String title, final String description,
+                                        Response.Listener<JSONObject> successListener,
+                                        Response.ErrorListener errorListener) {
+        String URL = ApiURL.API_ALL_QUESTIONNAIRE;
+
+        JSONObject JRequestObject = new JSONObject();
+        try {
+            JRequestObject.put("title", title);
+            JRequestObject.put("description", description);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.POST, URL, JRequestObject, successListener, errorListener);
 
         coreApi.getRequestQueue().getCache().clear();
         coreApi.addToRequestQueue(request);

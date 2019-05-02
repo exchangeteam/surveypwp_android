5821067ce9cc59a4f568918f57d76c126b4d6501
berke-esmer-2015400021 <berke.esmer@boun.edu.tr>
2019-05-02 10:26:04 +0300

    New URL for question get is added.

--- app/src/main/java/com/berkethetechnerd/surveypwp/helper/ApiURL.java
+++ app/src/main/java/com/berkethetechnerd/surveypwp/helper/ApiURL.java
@@ -1,11 +1,14 @@
 package com.berkethetechnerd.surveypwp.helper;
 
 /**
- * All constants are stored in this class.
+ * All URLs are stored in this class.
  */
 public class ApiURL {
 
-    public static final String API_BASE_URL = "http://45.76.39.46:5000/";
+    private static final String API_BASE_URL = "http://45.76.39.46:5000/";
     public static final String API_ALL_QUESTIONNAIRE = API_BASE_URL + "api/questionnaires/";
 
+    public static String API_GET_QUESTIONS(int questionnaire_id) {
+        return API_ALL_QUESTIONNAIRE + questionnaire_id + "/questions/";
+    }
 }

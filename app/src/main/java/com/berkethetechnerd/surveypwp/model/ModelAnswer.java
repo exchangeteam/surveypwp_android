a49dc4d2474b41f8eb1e738807c4ad98e89081b1
berke-esmer-2015400021 <berke.esmer@boun.edu.tr>
2019-05-02 18:17:08 +0300

    Model answer is shaped by adding username and question id.

--- app/src/main/java/com/berkethetechnerd/surveypwp/model/ModelAnswer.java
+++ app/src/main/java/com/berkethetechnerd/surveypwp/model/ModelAnswer.java
@@ -12,20 +12,28 @@ public class ModelAnswer {
     private int questionnaire_id;
 
     @Expose
+    private int question_id;
+
+    @Expose
+    private String userName;
+
+    @Expose
     private String title;
 
     @Expose
     private String description;
 
-    @Expose
+    @SerializedName("content")
     private String answer;
 
     @SerializedName("@controls")
     private HyperControls controls;
 
-    public ModelAnswer(int id, int questionnaire_id, String title, String description, String answer, HyperControls controls) {
+    public ModelAnswer(int id, int questionnaire_id, int question_id, String userName, String title, String description, String answer, HyperControls controls) {
         this.id = id;
         this.questionnaire_id = questionnaire_id;
+        this.question_id = question_id;
+        this.userName = userName;
         this.title = title;
         this.description = description;
         this.answer = answer;
@@ -56,12 +64,28 @@ public class ModelAnswer {
         return controls;
     }
 
+    public int getQuestion_id() {
+        return question_id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
     public void setId(int id) {
         this.id = id;
     }
 
     public void setQuestionnaire_id(int questionnaire_id) {
         this.questionnaire_id = questionnaire_id;
+    }
+
+    public void setQuestion_id(int question_id) {
+        this.question_id = question_id;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
     }
 
     public void setTitle(String title) {
